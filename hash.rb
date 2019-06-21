80d74c8a04b2823e2f37ba58f1b552a24718280b
TemirlanIdrissov <51860679+TemirlanIdrissov@users.noreply.github.com>
2019-06-21 23:09:18 +0600

    Add files via upload

---
+++ hash.rb
@@ -0,0 +1,6 @@
+months = {"jan" => 31, "feb" => 28, "mar" => 31, "apr" => 30, "may" => 31, "june" => 30, "july" => 31, "aug" => 31, "sep" => 30, "oct" => 31, "nov" => 30, "dec" => 31}
+months.each do |months, days|
+  puts "Result: #{months} - #{days}" 
+  if days == 30
+end
+
