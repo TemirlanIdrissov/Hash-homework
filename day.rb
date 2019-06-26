130b1f02a39fb887e29ca0a3dca675e43074f0a3
TemirlanIdrissov <51860679+TemirlanIdrissov@users.noreply.github.com>
2019-06-26 20:11:51 +0500

    Update day.rb

--- day.rb
+++ day.rb
@@ -8,7 +8,7 @@ puts 'Введите год:'
 year = gets.chomp.to_i
 
 days_in_a_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-days_in_a_month[1] = 29 if year % 400 == 0
+days_in_a_month[1] = 29 if year % 400 == 0. && year % 4 == 0 && year % 100 != 0
 
 sum = 0
 for i in 0..month-2
