f1b6ef343eb58ec39d5b6351616c742d1a37febf
TemirlanIdrissov <51860679+TemirlanIdrissov@users.noreply.github.com>
2019-06-26 20:26:19 +0600

    Add files via upload

---
+++ alph.rb
@@ -0,0 +1,18 @@
+alphabet = ('A'..'Z').to_a
+
+vowels = ['A', 'E', 'I', 'O', 'U', 'Y']
+
+h = {}
+
+alphabet.each.with_index(1) do |char, index|
+	if vowels.include?(char)
+		h[char] = index 
+		puts h
+	end
+end
+
+
+
+
+
+
